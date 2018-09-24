7996ea533273612fee7bec52d7c8eeba24c7010c
Blackout <trapmaster2010@hotmail.com>
2018-09-25 03:55:57 +0700

    edit seperate BUG: changes of brightness cause binary cant detect cell

--- Granule Seperate all pic.py
+++ Granule Seperate all pic.py
@@ -9,12 +9,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
 import skimage
-from skimage.io import imread , imshow
+from skimage.io import imread , imshow, imsave
 from skimage import color
 from skimage import data
 from skimage import measure
 from skimage.filters import threshold_mean
 from skimage.measure import label, regionprops
+from skimage.draw import polygon
 import os
 
 PATH = 'segmentation_WBC-master/'
@@ -27,7 +28,7 @@ for file in all_files:
         
         #find contour ( edge )
         contours = measure.find_contours(invert_img, 0.7)
-        
+        '''
         # save contour plot
         fig, ax = plt.subplots()
         ax.imshow(invert_img, cmap=plt.cm.gray)
@@ -39,11 +40,23 @@ for file in all_files:
         ax.set_yticks([])
         plt.tight_layout()
         plt.savefig('contour_'+file+'.png',bbox_inches='tight',pad_inches=0)
+        '''
+        for n, contour in enumerate(contours):
+            pr = np.array([p[0] for p in contour])
+            pc = np.array([p[1] for p in contour])
+            row = list(map(np.uint8,contour[:,0]))
+            col = list(map(np.uint8,contour[:,1]))
+            rr, cc = polygon(pr,pc)
+            #print(pr,',',pc)
+            invert_img[rr,cc] = 255
         
         #convert gray -> binary
         thresh = threshold_mean(invert_img)
         binary = invert_img > thresh
+        binary = binary*255
+        imsave(PATH+'/segmentation/'+'out_'+file+'.png',binary)
         
         #label object in image
         label_img = label(binary)
         props = regionprops(label_img)
+        
