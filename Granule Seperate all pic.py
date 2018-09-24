1c6328fe23c6a2c73f7949d15892eeb7908b69d1
Blackout <trapmaster2010@hotmail.com>
2018-09-25 03:37:58 +0700

    add seperate code

---
+++ Granule Seperate all pic.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 23:40:26 2018
+
+@author: 58011256
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage as ndi
+import skimage
+from skimage.io import imread , imshow
+from skimage import color
+from skimage import data
+from skimage import measure
+from skimage.filters import threshold_mean
+from skimage.measure import label, regionprops
+import os
+
+PATH = 'segmentation_WBC-master/'
+all_files = next(os.walk(PATH+'Dataset 1'))[2]
+for file in all_files:
+    if file.find('.bmp') > 0:
+        img = color.rgb2grey(imread('segmentation_WBC-master/Dataset 1/'+file))
+        #invert black->white , white->black
+        invert_img = 1-img
+        
+        #find contour ( edge )
+        contours = measure.find_contours(invert_img, 0.7)
+        
+        # save contour plot
+        fig, ax = plt.subplots()
+        ax.imshow(invert_img, cmap=plt.cm.gray)
+        
+        for n, contour in enumerate(contours):
+            ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+        ax.axis('image')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        plt.tight_layout()
+        plt.savefig('contour_'+file+'.png',bbox_inches='tight',pad_inches=0)
+        
+        #convert gray -> binary
+        thresh = threshold_mean(invert_img)
+        binary = invert_img > thresh
+        
+        #label object in image
+        label_img = label(binary)
+        props = regionprops(label_img)
